543bcd3c31cc66dc41e33598e222491c7d21a644
Roman Repin <romarepin23rus@gmail.com>
2023-10-04 00:42:55 +0300

    [test] add test login by click personal cabinet button on main page

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -26,4 +26,25 @@ public class LoginTest extends BaseTest {
         personalCabinetPage.checkPersonalCabinetPage(testUserName, testUserEmail);
     }
 
+    @Test
+    public void loginByPersonalCabinetButton() {
+        //given
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        mainPage.checkMainPage(false);
+
+        //when
+        mainPage.clickPersonalCabinetButton();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.checkLoginPage();
+        loginPage.fillLoginPage(testUserEmail, testUserPassword);
+        loginPage.clickLoginButton();
+
+        //then
+        mainPage.checkMainPage(true);
+        mainPage.clickPersonalCabinetButton();
+        PersonalCabinetPage personalCabinetPage = new PersonalCabinetPage(driver);
+        personalCabinetPage.checkPersonalCabinetPage(testUserName, testUserEmail);
+    }
+
 }
